52e9bbf549438fb7904f18b93d7adbb3f18b41e1
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2020-10-09 00:37:42 +0200

    Initial commit containing primary files for all 3 programs.

---
+++ market/src/main/java/com/ctaljaar/market/Market.java
@@ -0,0 +1,9 @@
+package com.ctaljaar.market;
+
+import java.io.*;
+
+public class Market {
+    public static void main(String[] args) {
+		System.out.println("Market works");
+    }
+}
---
+++ router/src/main/java/com/ctaljaar/router/Router.java
@@ -0,0 +1,10 @@
+package com.ctaljaar.router;
+
+import java.io.*;
+
+public class Router {
+
+	public static void main(String[] arg) {
+		System.out.println("Router works");
+	}
+}
