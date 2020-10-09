39812d2389cbd99d9b5f244992e1bd4103814b8a
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2020-10-09 21:32:46 +0200

    Cleaned up the maven pom files to be consistent and always include sqllite and lombok.

--- router/src/main/java/com/ctaljaar/router/Router.java
+++ router/src/main/java/com/ctaljaar/router/Router.java
@@ -10,11 +10,17 @@ public class Router {
 		ServerSocket marketSocket = new ServerSocket(5001);
 		Socket clientSocket;
 
+		// int activeConnections = 0;
+
 		System.out.println("Waiting for connection on port 5001");
 		while (true) {
 			// Code will stop here until a connection is made. Similar to waiting for user input.
 			clientSocket = marketSocket.accept();
 			System.out.println("Connection made");
+
+			// Just code to test that multiple connections are accepted.
+			// activeConnections++;
+			// System.out.println("Connections made: " + activeConnections);
 		}
 	}
 }
