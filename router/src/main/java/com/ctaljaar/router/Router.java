c42271982a0152e6f64a737661f24c75ea8dce52
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2020-10-09 17:37:12 +0200

    Router now waits for connections on port 5001

--- router/src/main/java/com/ctaljaar/router/Router.java
+++ router/src/main/java/com/ctaljaar/router/Router.java
@@ -1,10 +1,19 @@
 package com.ctaljaar.router;
 
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Router {
 
-	public static void main(String[] arg) {
-		System.out.println("Router works");
+	public static void main(String[] arg) throws IOException{
+		ServerSocket marketSocket = new ServerSocket(5001);
+		Socket clientSocket;
+
+		System.out.println("Waiting for connection on port 5001");
+		while (true) {
+			clientSocket = marketSocket.accept();
+			System.out.println("Connection made");
+		}
 	}
 }
