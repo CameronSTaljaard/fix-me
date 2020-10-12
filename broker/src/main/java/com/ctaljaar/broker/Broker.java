f435fb54e48b6d1314f505d1dc5ce509302905f8
brands <brandonlee.swanepoel@gmail.com>
2020-10-12 21:29:46 +0200

    Broker Socket

--- broker/src/main/java/com/ctaljaar/broker/Broker.java
+++ broker/src/main/java/com/ctaljaar/broker/Broker.java
@@ -1,9 +1,21 @@
 package com.ctaljaar.broker;
 
 import java.io.*;
+import java.net.Socket;
 
 public class Broker {
-	public static void main(String[] args) {
-		System.out.println("Broker Works");
+	public static void main(String[] args) throws Exception {
+		String ip = "localhost";
+		int port = 5000;
+		Socket brokerSocket = new Socket(ip, port);
+		DataOutputStream outputStream = new DataOutputStream(brokerSocket.getOutputStream());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while(true){
+			String readLine = br.readLine();
+			outputStream.writeUTF(readLine);
+			if(readLine.equalsIgnoreCase("exit"))
+				break;
+		}
+		brokerSocket.close();
 	}
 }
