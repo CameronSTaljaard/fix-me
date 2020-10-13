0a06c0fe7f86ca9885119ddb04223f9653c9f181
brands <brandonlee.swanepoel@gmail.com>
2020-10-13 13:35:34 +0200

    Changed the Broker code

--- broker/src/main/java/com/ctaljaar/broker/Broker.java
+++ broker/src/main/java/com/ctaljaar/broker/Broker.java
@@ -6,35 +6,19 @@ import java.net.Socket;
 public class Broker {
 	public static void main(String[] args) throws Exception {
 		String readLine;
-		int i = 0;
 		String ip = "localhost";
 		int port = 5000;
 		Socket brokerSocket = new Socket(ip, port);
-		DataOutputStream outputStream = new DataOutputStream(brokerSocket.getOutputStream());
+		PrintWriter outputStream = new PrintWriter(brokerSocket.getOutputStream(), true);
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		boolean done = false;
-		while (!done) {
+		while (true) {
 			readLine = br.readLine();
-			/* The first message that the Broker sends will also send 1 byte to the Router the following messages will
-			 send 2 bytes to the Router with the input of the Broker*/
-			if (i == 0) {
-				outputStream.writeByte(1);// Sends 1 byte
-				outputStream.writeUTF(readLine);// Sends the input of the Broker
-				outputStream.flush();
-				i++;
-				if (readLine.equalsIgnoreCase("exit"))
-					break;
-
-			} else if (i == 1) {
-				outputStream.writeByte(2);// Sends 2 bytes
-					outputStream.writeUTF(readLine);// Sends the input of the Broker
-					if (readLine.equalsIgnoreCase("exit"))
-						break;
-			} else {
-				done = true;
-			}
+			outputStream.println(readLine);// Sends the input of the Broker
+			// if the Broker sends 'exit' then this Socket will close
+			if (readLine.equalsIgnoreCase("exit"))
+				break;
 		}
 		brokerSocket.close();
 
