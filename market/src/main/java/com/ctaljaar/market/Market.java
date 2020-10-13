0d8ed86a860cd1bf6a78d729e29da066f2a43c92
CameronSTaljaard <46622484+CameronSTaljaard@users.noreply.github.com>
2020-10-13 21:31:29 +0200

    Commiting a basic Market

--- market/src/main/java/com/ctaljaar/market/Market.java
+++ market/src/main/java/com/ctaljaar/market/Market.java
@@ -1,9 +1,23 @@
 package com.ctaljaar.market;
 
 import java.io.*;
+import java.net.Socket;
 
 public class Market {
-    public static void main(String[] args) {
-		System.out.println("Market works");
-    }
+	public static void main(String[] args) throws Exception {
+		String ip = "localhost";
+		int port = 5001;
+		String input;
+		Socket marketSocket = new Socket(ip, port);
+		PrintWriter outputStream = new PrintWriter(marketSocket.getOutputStream(), true);
+		BufferedReader inputStream = new BufferedReader(new InputStreamReader(marketSocket.getInputStream()));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		while (true) {
+			input = inputStream.readLine();
+			if (input.equalsIgnoreCase("exit"))
+				break;
+		}
+		marketSocket.close();
+	}
 }
