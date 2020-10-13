4e3bedf8595da41a82747b3d89af000c05362563
brands <brandonlee.swanepoel@gmail.com>
2020-10-13 09:35:10 +0200

    Broker connects to the Router

--- broker/src/main/java/com/ctaljaar/broker/Broker.java
+++ broker/src/main/java/com/ctaljaar/broker/Broker.java
@@ -5,17 +5,38 @@ import java.net.Socket;
 
 public class Broker {
 	public static void main(String[] args) throws Exception {
+		String readLine;
+		int i = 0;
 		String ip = "localhost";
 		int port = 5000;
 		Socket brokerSocket = new Socket(ip, port);
 		DataOutputStream outputStream = new DataOutputStream(brokerSocket.getOutputStream());
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		while(true){
-			String readLine = br.readLine();
-			outputStream.writeUTF(readLine);
-			if(readLine.equalsIgnoreCase("exit"))
-				break;
+
+		boolean done = false;
+		while (!done) {
+			readLine = br.readLine();
+			/* The first message that the Broker sends will also send 1 byte to the Router the following messages will
+			 send 2 bytes to the Router with the input of the Broker*/
+			if (i == 0) {
+				outputStream.writeByte(1);// Sends 1 byte
+				outputStream.writeUTF(readLine);// Sends the input of the Broker
+				outputStream.flush();
+				i++;
+				if (readLine.equalsIgnoreCase("exit"))
+					break;
+
+			} else if (i == 1) {
+				outputStream.writeByte(2);// Sends 2 bytes
+					outputStream.writeUTF(readLine);// Sends the input of the Broker
+					if (readLine.equalsIgnoreCase("exit"))
+						break;
+			} else {
+				done = true;
+			}
 		}
 		brokerSocket.close();
+
 	}
 }
