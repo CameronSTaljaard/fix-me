07a04c91331ef45a4c4b4ebc1adca0cd7c3ed8db
brands <brandonlee.swanepoel@gmail.com>
2020-10-13 13:36:38 +0200

    Creates a broker thread when a new broker joins

--- router/src/main/java/com/ctaljaar/router/Router.java
+++ router/src/main/java/com/ctaljaar/router/Router.java
@@ -1,50 +1,30 @@
 package com.ctaljaar.router;
 
-import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-
-import com.ctaljaar.router.model.ConnectionHandler;
-
 public class Router {
+
+	public static ArrayList<String> onlineBrokers = new ArrayList<>();
 
 	public static void main(String[] arg) throws Exception {
 
-		int port = 5000;
-		ArrayList<String> onlineBrokers = new ArrayList<>();
-		ServerSocket serverSocket = new ServerSocket(port);
+		int brokerPort = 5000;
+		int marketPort = 5001;
 
-		Socket ss = serverSocket.accept();
+		ServerSocket brokerServerSocket = new ServerSocket(brokerPort);
+		// ServerSocket marketServerSocket = new ServerSocket(marketPort);
+
 		System.out.println("connected");
-		DataInputStream dataInputStream = new DataInputStream(ss.getInputStream());
-		boolean done = false;
-		while (!done) {
-			byte messageType = dataInputStream.readByte();// Stream reads the bytes that was sent by the Broker
-			String brokerMessage = dataInputStream.readUTF(); // Stream reads the message that was sent by the Broker
-
-			if (messageType == 1) // The first message from a new Broker will go in here
-			{
-				// Adds the New Broker to the List of online Brokers
-				onlineBrokers.add(ConnectionHandler.generateID());
-				System.out.println("New Broker joined");
-				System.out.println(onlineBrokers);
-				System.out.println("Broker message = " + brokerMessage);
-			}
-
-			else if (messageType == 2) // The rest of the messages from the Broker will go in here
-			{
-
-				System.out.println("Broker message = " + brokerMessage);
-				if (brokerMessage.equalsIgnoreCase("exit"))
-					break;
-			} else {
-				done = true;
-
+		while (true) {
+			//Makes connection with the new broker socket
+			Socket brokerSocket = brokerServerSocket.accept();
+			System.out.println("New Broker joined");
+			//Creates a new Thread for the Broker
+			new ServerThread(brokerSocket).start();
+			if(brokerSocket.isClosed()){
+				brokerServerSocket.close();
 			}
 		}
-		ss.close();
-		serverSocket.close();
-
 	}
 }
---
+++ router/src/main/java/com/ctaljaar/router/ServerThread.java
@@ -0,0 +1,53 @@
+package com.ctaljaar.router;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import com.ctaljaar.router.model.ConnectionHandler;
+
+/*You have to use Thread to make a new Thread each instance of this class 
+because Runnable only makes one Thread
+*/
+public class ServerThread extends Thread {
+
+	Socket brokerSocket;
+	ServerSocket marketServerSocket;
+
+	public ServerThread(Socket brokerSocket) {
+		this.brokerSocket = brokerSocket;
+		// this.marketSocket = marketSocket;
+	}
+
+	@Override
+	public void run() {
+		try {
+			String brokerID = ConnectionHandler.generateID();
+			// Adds the New Broker to the List of online Brokers
+			Router.onlineBrokers.add(brokerID);
+			// Reads the brokers input on the terminal
+			BufferedReader brokerInput = new BufferedReader(new InputStreamReader(brokerSocket.getInputStream()));
+			String brokerMessage;
+			while (true) {
+				brokerMessage = brokerInput.readLine(); // BufferedReader reads the message that was sent by the Broker
+				System.out.println("Broker message = " + brokerMessage);
+
+				// if the Broker send 'exit' then the thread will end
+				if (brokerMessage.equalsIgnoreCase("exit")) {
+					// Removes this broker from the online brokers
+					removeBrokerFromOnlineList(brokerID);
+					break;
+				}
+			}
+			brokerSocket.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	void removeBrokerFromOnlineList(String brokerID) {
+		int index = Router.onlineBrokers.indexOf(brokerID);
+		Router.onlineBrokers.remove(index);
+		System.out.println(Router.onlineBrokers);
+	}
+}
