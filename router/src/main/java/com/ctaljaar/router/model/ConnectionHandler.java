e3876ed2c0e749e33077d2ae2a84e3c05361b215
brands <brandonlee.swanepoel@gmail.com>
2020-10-13 09:33:17 +0200

    Made the Class name start with an uppercase

---
+++ router/src/main/java/com/ctaljaar/router/model/ConnectionHandler.java
@@ -0,0 +1,21 @@
+package com.ctaljaar.router.model;
+
+import java.nio.charset.Charset;
+import java.util.Random;
+import java.lang.StringBuilder;
+
+public class ConnectionHandler {
+
+	public static String generateID() {
+		String allowedCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        StringBuilder code = new StringBuilder();
+		Random rand = new Random();
+		
+        while (code.length() < 6) {
+            int index = (int) (rand.nextFloat() * allowedCharacters.length());
+            code.append(allowedCharacters.charAt(index));
+        }
+        String uniqueID = code.toString();
+        return uniqueID;
+	}
+}
