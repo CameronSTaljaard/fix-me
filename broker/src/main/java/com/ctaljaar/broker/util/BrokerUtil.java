25467c2bda9a295ba226167114853318981a0355
brands <brandonlee.swanepoel@gmail.com>
2020-10-15 22:42:10 +0200

    print online Markets to the Broker terminal

--- broker/src/main/java/com/ctaljaar/broker/util/BrokerUtil.java
+++ broker/src/main/java/com/ctaljaar/broker/util/BrokerUtil.java
@@ -2,6 +2,7 @@ package com.ctaljaar.broker.util;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class BrokerUtil {
 
@@ -18,6 +19,45 @@ public class BrokerUtil {
         }
         System.out.println("");
         System.out.println("-----------End of list---------");
+    }
+
+    public static void printOnlineMarkets(BufferedReader routerInput) throws IOException {
+        int i = 0;
+        String routerMessage;
+        System.out.println("---------Online Markets-------");
+        System.out.println("");
+
+        // Reads the data from the router
+        while ((routerMessage = routerInput.readLine()) != null) {
+            // Prints a space between the different markets
+            if (i == 7) {
+                System.out.println("");
+                i = 0;
+            }
+
+            if (routerMessage.equalsIgnoreCase("End of list"))
+                break;
+            System.out.println(routerMessage);
+            i++;
+
+        }
+        System.out.println("-----------End of list---------");
+    }
+
+    public static void checkBrokerMessage(String readLine, PrintWriter outputStream, BufferedReader routerInput)
+            throws IOException {
+        outputStream.println(readLine);// Sends the input of the Broker
+        // if the Broker sends 'list' then Router will send all the online Brokers
+        if (readLine.equalsIgnoreCase("list")) {
+            // Prints the Online Brokers
+            BrokerUtil.printOnlineBrokers(routerInput);
+        }
+        if (readLine.equalsIgnoreCase("markets")) {
+            // Prints the Online Brokers
+            BrokerUtil.printOnlineMarkets(routerInput);
+
+        }
+
     }
 
 }
