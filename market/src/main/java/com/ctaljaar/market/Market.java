6330f5fd7de62e31ad8e49f62456cc6e54605501
brands <brandonlee.swanepoel@gmail.com>
2020-10-15 22:43:09 +0200

    Asks for market type

--- market/src/main/java/com/ctaljaar/market/Market.java
+++ market/src/main/java/com/ctaljaar/market/Market.java
@@ -6,16 +6,21 @@ import java.net.Socket;
 public class Market {
 	public static void main(String[] args) throws Exception {
 		String ip = "localhost";
+		String readLine;
 		int port = 5001;
-		String input;
+		// String input,stockName;
 		Socket marketSocket = new Socket(ip, port);
 		PrintWriter outputStream = new PrintWriter(marketSocket.getOutputStream(), true);
 		BufferedReader inputStream = new BufferedReader(new InputStreamReader(marketSocket.getInputStream()));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		BufferedReader terminalInput = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("What is the market type?");
+		readLine = terminalInput.readLine();
+		outputStream.println("Stock: " + readLine);
+		System.out.println("Thank you!");
+		
 		while (true) {
-			input = inputStream.readLine();
-			if (input.equalsIgnoreCase("exit"))
+
+			if (readLine.equalsIgnoreCase("exit"))
 				break;
 		}
 		marketSocket.close();
