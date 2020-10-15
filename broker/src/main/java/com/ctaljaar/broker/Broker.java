9a9f0b6e6b7701fc64c47d0e7986a351c32cb815
brands <brandonlee.swanepoel@gmail.com>
2020-10-15 10:01:43 +0200

    Asks for List of online brokers and prints them

--- broker/src/main/java/com/ctaljaar/broker/Broker.java
+++ broker/src/main/java/com/ctaljaar/broker/Broker.java
@@ -2,19 +2,35 @@ package com.ctaljaar.broker;
 
 import java.io.*;
 import java.net.Socket;
+import java.sql.Connection;
 
 public class Broker {
 	public static void main(String[] args) throws Exception {
 		String readLine;
+		String routerMessage;
 		String ip = "localhost";
 		int port = 5000;
 		Socket brokerSocket = new Socket(ip, port);
 		PrintWriter outputStream = new PrintWriter(brokerSocket.getOutputStream(), true);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader terminalInput = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader routerInput = new BufferedReader(new InputStreamReader(brokerSocket.getInputStream()));
 
-		while (true) {;
-			readLine = br.readLine();
+		while (true) {
+			readLine = terminalInput.readLine();
 			outputStream.println(readLine);// Sends the input of the Broker
+
+			// if the Broker sends 'list' then Router will send all the online Brokers
+			if (readLine.equalsIgnoreCase("list")) {
+				System.out.println("-------Connected brokers------");
+				System.out.println("");
+
+				// Reads the data from the router
+				routerMessage = routerInput.readLine();
+				System.out.println("Broker: " + routerMessage);
+				System.out.println("");
+				System.out.println("-----------End of list---------");
+			}
+			// outerInput
 			// if the Broker sends 'exit' then this Socket will close
 			if (readLine.equalsIgnoreCase("exit"))
 				break;
