2c79ae1b278ec8ba0766abfaf52dc0a13263dcfe
brands <brandonlee.swanepoel@gmail.com>
2020-10-15 22:48:33 +0200

    Creates the instance of the market

--- router/src/main/java/com/ctaljaar/router/market/MarketThread.java
+++ router/src/main/java/com/ctaljaar/router/market/MarketThread.java
@@ -1,11 +1,14 @@
 package com.ctaljaar.router.market;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import com.ctaljaar.router.util.RouterUtil;
 import com.ctaljaar.router.Router;
-import com.ctaljaar.router.util.Connection;
+import com.ctaljaar.router.util.MarketUtil;
 
 /*You have to use Thread to make a new Thread each instance of this class 
 because Runnable only makes one Thread
@@ -22,24 +25,40 @@ public class MarketThread extends Thread {
 	@Override
 	public void run() {
 		try {
+			MarketUtil thisMarket = null;
 			String marketID = RouterUtil.generateID();
 			System.out.println("Market joined with ID: " + marketID);
-			Connection thisMarket = new Connection(marketSocket, marketID);
-			Router.onlineMarkets.add(thisMarket);
+			// Connection thisMarket = new Connection(marketSocket, marketID);
+			BufferedReader marketInput = new BufferedReader(new InputStreamReader(marketSocket.getInputStream()));
+			PrintWriter outputStream = new PrintWriter(marketSocket.getOutputStream(), true);
+
+			String marketMessage;
 
 			while (true) {
-				break;
+				marketMessage = marketInput.readLine();
+				if (marketMessage != null) {
+					if (marketMessage.contains("Stock")) {
+						thisMarket = new MarketUtil(marketID, marketMessage);
+						Router.onlineMarkets.add(thisMarket);
+					}
+
+					if (marketMessage.equalsIgnoreCase("exit")) {
+						// Removes this broker from the online brokers
+						removeMarketFromOnlineList(thisMarket);
+						break;
+					}
+				}
 			}
-			
+
 			marketSocket.close();
 		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
 
-	void removeMarketFromOnlineList(Connection thisMarket) {
-		int index = Router.onlineMarkets.indexOf(thisMarket);
-		Router.onlineMarkets.remove(index);
-		System.out.println(Router.onlineMarkets);
+	void removeMarketFromOnlineList(MarketUtil thisMarket) {
+	int index = Router.onlineMarkets.indexOf(thisMarket);
+	Router.onlineMarkets.remove(index);
+	System.out.println(Router.onlineMarkets);
 	}
 }
